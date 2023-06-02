e69f77d9a3aa91466b108ad24e4b5bf85a366f41
Lol5ly <adiouaneleila18@gmail.com>
2023-06-01 18:02:46 -0700

    task101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
 global    main
           extern    printf
 main:
-	  mov   edi, message
-	  eax
+	  mov   edi, format
+	  xor   eax, eax
 	  call  printf
 	  mov 	eax, 0
-	  return
-message: db `Hello, Holberton\n`,0
+	  ret
+format: db `Hello, Holberton\n`,0
